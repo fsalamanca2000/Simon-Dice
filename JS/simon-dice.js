ff597f91cbc34ec06da03179d412b4cfb3c41fae
Felipe Salamanca <juanfe689@gmail.com>
2019-11-09 21:48:28 -0500

    Clase 3 Variables Numericas

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -1,11 +1,27 @@
-var nombre= 'Felipe', apellido= 'Salamanca';
+var edad = 27;
 
-var nombreEnMayusculas = nombre.toUpperCase();
-var apellidoEnMayusculas = apellido.toLowerCase();
+/* edad = edad +1; */
+edad += 1;
 
-var primeraLetraDelNombre = nombre.charAt(0);
-var cantidadDeLetrasDelNombre = nombre.length;
+var peso = 75;
 
-var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+/* peso = peso - 2; */
+peso-= 2
 
-var str = nombre.substr(1, 2);
+var sandwich = 1
+
+peso += sandwich
+
+var jugarAlFutbol = 3
+
+peso -= jugarAlFutbol
+
+var precioDeVino = 200.3
+
+var total = Math.round(precioDeVino * 100 * 3) / 100
+var totalStr =total.toFixed(2)
+var total2 = parseFloat(totalStr)
+
+var pizza = 8
+var persona = 2
+var cantidadPorcionePersona = pizza / persona
