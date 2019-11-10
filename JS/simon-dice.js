416829b0f5c54d951e27eb9da7f3fd767bb463cb
Felipe Salamanca <juanfe689@gmail.com>
2019-11-09 19:51:09 -0500

    Clase 2

---
+++ JS/simon-dice.js
@@ -0,0 +1,11 @@
+var nombre= 'Felipe', apellido= 'Salamanca';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMayusculas = apellido.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length;
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+
+var str = nombre.substr(1, 2);
