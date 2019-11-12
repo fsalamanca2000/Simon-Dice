5b8b3ff39f1d94b963d305ea1093784a0e0906a4
Felipe Salamanca <juanfe689@gmail.com>
2019-11-11 22:12:06 -0500

    Destructurando Objetos

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -4,19 +4,30 @@ var caracteristicasFelipe ={
   edad: 18
 }
 
-var caracteristicasDario = {
+ var caracteristicasDario = {
   nombre: 'Dario',
   apellido: 'Mendez',
   edad: 25
 }
-function imprimerNombreEnMayuscula({ nombre })
+/*function imprimerNombreEnMayuscula(persona)
 {
+  var { nombre } = persona
   console.log(nombre.toUpperCase())
 }
 
 imprimerNombreEnMayuscula(caracteristicasFelipe)
-imprimerNombreEnMayuscula(caracteristicasDario)
+imprimerNombreEnMayuscula(caracteristicasDario) */
 //Si utilizo {} dentro de los () de una funcion nos permite crear un atributo directamente 
-imprimerNombreEnMayuscula({nombre: 'Pepito'})
+//imprimerNombreEnMayuscula({nombre: 'Pepito'})
 //Si utilizo un atributo que no sea el correspondiente no se va a ejecutar
-imprimerNombreEnMayuscula({ apellido: 'Gomez'})
+//imprimerNombreEnMayuscula({ apellido: 'Gomez'})
+
+function imprimirNombreYEdad(persona)
+{
+  var {nombre} = persona
+  var {edad} = persona
+  console.log('Hola soy ' + nombre + ' y tengo ' + edad + ' años' )
+}
+
+imprimirNombreYEdad(caracteristicasFelipe)
+imprimirNombreYEdad(caracteristicasDario)
