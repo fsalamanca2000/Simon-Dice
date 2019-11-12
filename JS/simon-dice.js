6c1ecaf934ed7426458269a8829554895c00efc2
Felipe Salamanca <juanfe689@gmail.com>
2019-11-11 21:00:03 -0500

    Creacion de objetos (variables con varios atributos asignados) y añadiendolos a una funcion

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -1,10 +1,22 @@
-var nombre = 'Felipe'
-
-function imprimerNombreEnMayuscula(nombre)
-{
-  nombre = nombre.toUpperCase()
-  console.log(nombre)
+var caracteristicasFelipe ={
+  nombre: 'Felipe',
+  apellido: 'Salamanca',
+  edad: 18
 }
 
-imprimerNombreEnMayuscula(nombre)
+var caracteristicasDario = {
+  nombre: 'Dario',
+  apellido: 'Mendez',
+  edad: 25
+}
+function imprimerNombreEnMayuscula({ nombre })
+{
+  console.log(nombre.toUpperCase())
+}
 
+imprimerNombreEnMayuscula(caracteristicasFelipe)
+imprimerNombreEnMayuscula(caracteristicasDario)
+//Si utilizo {} dentro de los () de una funcion nos permite crear un atributo directamente 
+imprimerNombreEnMayuscula({nombre: 'Pepito'})
+//Si utilizo un atributo que no sea el correspondiente no se va a ejecutar
+imprimerNombreEnMayuscula({ apellido: 'Gomez'})
