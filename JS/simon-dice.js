bf1952eb4ad41c290197fc8096d2f2c320c35b0f
Felipe Salamanca <juanfe689@gmail.com>
2019-11-13 18:28:21 -0500

    Funciones condicionales (if y else)

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -1,9 +1,66 @@
-var x = 4 , y = '4'
- 
-var sacha = {
-  nombre: 'Sacha'
+var felipe ={
+  nombre: 'Felipe',
+  apellido: 'Salamanca',
+  edad: 18,
+  ingeniero: true,
+  cocinero: false,
+  cantante: false,
+  dj: false,
+  guitarrista: false,
+  drone: true
 }
 
-var persona = {
-  nombre: 'Pepe'
+function imprimirProfesiones(persona){
+  console.log(`${persona.nombre} es:`)
+  if(persona.ingeniero === true)
+  {
+    console.log('Ingeniero')
+  } else{
+    console.log('No es ingeniero')
+  }
+
+  if(persona.cocinero === true)
+  {
+    console.log('Cocinero')
+  } else{
+    console.log('No es cocinero')
+  }
+  if(persona.cantante === true)
+  {
+    console.log('Cantante')
+  } else{
+    console.log('No es cantante')
+  }
+  if(persona.dj === true)
+  {
+    console.log('DJ')
+  } else{
+    console.log('No es DJ')
+  }
+  if(persona.guitarrista === true)
+  {
+    console.log('Guitarrista')
+  } else{
+    console.log('No es guitarrista')
+  }
+  if(persona.drone === true)
+  {
+    console.log('Piloto de Drone')
+  } else{
+    console.log('No es pelito de drone')
+  }
 }
+
+imprimirProfesiones(felipe)
+
+function imprimirSiEsMayor(persona) {
+  if(persona.edad >= 18){
+    console.log('Usted es mayor de edad')
+  }
+  else
+  {
+    console.log('Usted no es mayor de edad')
+  }
+}
+
+imprimirSiEsMayor(felipe)
