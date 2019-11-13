221b183b5ef0eff184e7a8b1ba030e6f2c433331
Felipe Salamanca <juanfe689@gmail.com>
2019-11-13 17:54:26 -0500

    Comparativas con JavaScript usando == y ===

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -1,41 +1,9 @@
-var caracteristicasFelipe ={
-  nombre: 'Felipe',
-  apellido: 'Salamanca',
-  edad: 18
+var x = 4 , y = '4'
+ 
+var sacha = {
+  nombre: 'Sacha'
 }
 
- var caracteristicasDario = {
-  nombre: 'Dario',
-  apellido: 'Mendez',
-  edad: 25
-}
-/*function imprimerNombreEnMayuscula(persona)
-{
-  var { nombre } = persona
-  console.log(nombre.toUpperCase())
-}
-
-imprimerNombreEnMayuscula(caracteristicasFelipe)
-imprimerNombreEnMayuscula(caracteristicasDario) */
-//Si utilizo {} dentro de los () de una funcion nos permite crear un atributo directamente 
-//imprimerNombreEnMayuscula({nombre: 'Pepito'})
-//Si utilizo un atributo que no sea el correspondiente no se va a ejecutar
-//imprimerNombreEnMayuscula({ apellido: 'Gomez'})
-
-function imprimirNombreYEdad(persona)
-{
-  var {nombre} = persona
-  var {edad} = persona
-  console.log('Hola soy ' + nombre + ' y tengo ' + edad + ' años' )
-}
-
-imprimirNombreYEdad(caracteristicasFelipe)
-imprimirNombreYEdad(caracteristicasDario)
-
-
-function cumpleanos(persona){
-  return{
-    ...persona,
-    edad: persona.edad +1
-  }
+var persona = {
+  nombre: 'Pepe'
 }
