ca091ee22688ee24300eecf3121ce6bdbc89f353
Felipe Salamanca <juanfe689@gmail.com>
2019-11-16 21:42:56 -0500

    Aprendiendo sobre for

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -1,80 +1,23 @@
-var felipe ={
+var felipe = {
   nombre: 'Felipe',
-  apellido: 'Salamanca',
+  apellido:'Salamanca',
   edad: 18,
-  ingeniero: true,
-  cocinero: false,
-  cantante: false,
-  dj: false,
-  guitarrista: false,
-  drone: true
+  peso: 63
 }
 
-function imprimirProfesiones(persona){
-  console.log(`${persona.nombre} es:`)
-  if(persona.ingeniero === true)
-  {
-    console.log('Ingeniero')
-  } else{
-    console.log('No es ingeniero')
+console.log(`Al inicio del año ${felipe.nombre} pesa ${felipe.peso}kg`)
+const VARIACION_PESO = 0.2
+const aumentoPeso = (persona) => persona.peso += VARIACION_PESO
+const adelgazar = persona => persona.peso -= VARIACION_PESO
+for (var i = 1; i <= 365; i++) 
+{
+  var random = Math.random()
+  if (random < 0.25){
+    aumentoPeso(felipe)
   }
-
-  if(persona.cocinero === true)
-  {
-    console.log('Cocinero')
-  } else{
-    console.log('No es cocinero')
-  }
-  if(persona.cantante === true)
-  {
-    console.log('Cantante')
-  } else{
-    console.log('No es cantante')
-  }
-  if(persona.dj === true)
-  {
-    console.log('DJ')
-  } else{
-    console.log('No es DJ')
-  }
-  if(persona.guitarrista === true)
-  {
-    console.log('Guitarrista')
-  } else{
-    console.log('No es guitarrista')
-  }
-  if(persona.drone === true)
-  {
-    console.log('Piloto de Drone')
-  } else{
-    console.log('No es pelito de drone')
+  else if(random <0.5){
+    adelgazar(felipe)
   }
 }
 
-var juan = {
-  nombre: 'Juan',
-  edad: 15 
-}
-const mayoriaDeEdad = 18
-
-const esMayordeEdad = ({ edad })=> edad >= mayoriaDeEdad
-
- function imprimirSiEsMayor(persona) {
-  if(esMayordeEdad(persona)){
-    console.log('Usted es mayor de edad')
-  }
-  else if(esMenorDeEdad(persona)){
-    console.log('Usted es menor de edad')
-  }
-}
-const esMenorDeEdad = ({ edad }) => edad < mayoriaDeEdad
-
-function permitirAcceso(persona){
-  if (esMenorDeEdad(persona))
-  {
-    console.log('Acceso Denegado washin')
-  }
-  else{
-    console.log('Tiene acceso, siga por favor')
-  }
-}
+console.log(`Al final del año ${felipe.nombre} pesa ${felipe.peso.toFixed(2)}kg`)
