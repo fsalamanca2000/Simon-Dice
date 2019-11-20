e444c0fe3ccdb51096827787ac890bad413fe34e
Felipe Salamanca <juanfe689@gmail.com>
2019-11-19 23:51:53 -0500

    como funciona un clase en JavaScript con ejercicio añadido

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -1,11 +1,30 @@
-function Persona(nombre, apellido){
+function Persona(nombre, apellido, altura){
   this.nombre = nombre
   this.apellido = apellido
+  this.altura = altura
 }
 
 Persona.prototype.saludar = function (){
   console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
 }
-var felipe = new Persona('Felipe', 'Salamanca')
-var erika = new Persona('Erika', 'Luna')
-var arturo = new Persona('Arturo','Martinez')
+Persona.prototype.esAltoOBajo = function(){
+  if(this.altura >= 1.80){
+  console.log(`${this.nombre} Su altura es ${this.altura}mts por lo que es una persona alta`)
+  }
+  else if(this.altura > 1.69 && this.altura < 1.80){
+    console.log(`${this.nombre} Su altura es ${this.altura}mts por lo que no es ni alto ni bajo`)
+  }
+  else if(this.altura <= 1.69){
+    console.log(`${this.nombre} Su altura es ${this.altura}mts por lo que es una persona baja`)
+  }
+}
+var felipe = new Persona('Felipe', 'Salamanca', 1.74)
+var erika = new Persona('Erika', 'Luna', 1.65)
+var arturo = new Persona('Arturo','Martinez', 1.85)
+
+felipe.saludar()
+erika.saludar()
+arturo.saludar()
+felipe.esAltoOBajo()
+erika.esAltoOBajo()
+arturo.esAltoOBajo()
