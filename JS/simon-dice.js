042f9c2dd7078a77f78b4c965737b3cf4ce0375c
Felipe Salamanca <juanfe689@gmail.com>
2019-11-20 17:59:38 -0500

    This y su contexto siendo parte de window

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -7,14 +7,11 @@ function Persona(nombre, apellido, altura){
 Persona.prototype.saludar = function (){
   console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
 }
-Persona.prototype.esAltoOBajo = () => this.altura > 1.80
+Persona.prototype.esAltoOBajo = function() {
+  return this.altura > 1.80
+}
+
 var felipe = new Persona('Felipe', 'Salamanca', 1.74)
 var erika = new Persona('Erika', 'Luna', 1.65)
 var arturo = new Persona('Arturo','Martinez', 1.85)
 
-felipe.saludar()
-erika.saludar()
-arturo.saludar()
-felipe.esAltoOBajo()
-erika.esAltoOBajo()
-arturo.esAltoOBajo()
