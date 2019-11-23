3143d9e1fe5315f45a164aa0101c5c4c9a43df0c
Felipe Salamanca <juanfe689@gmail.com>
2019-11-22 19:13:12 -0500

    Hacer un Callback

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -1,41 +1,10 @@
-class Persona{
-  constructor(nombre, apellido, altura){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.altura = altura
-  }
-    saludar(fn) {
-      console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
-      if(fn){
-        fn(this.nombre, this.apellido)
-      }
-    }
-    esAltoOBajo(){
-      return this.altura > 1.80
-    }
-}
-class Desarrollador extends Persona{
-  constructor(nombre, apellido,altura){
-    super(nombre, apellido, altura)
-  }
-  saludar(fn){
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador`)
-    if(fn){
-      fn(this.nombre, this.apellido, true)
-    }
-  }
-}
-function responderSaludo(nombre, apellido, esDev){
-  console.log(`Buen dia ${nombre} ${apellido}`)
-  if(esDev){
-    console.log(`Ah mirá no sabia que sos un desarrollador/a`)
-  }
+const API_URL ='https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+
+const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
+const opts = { crossDomain: true}
+const onPeopleResponse = function (character){
+  console.log(`Hola yo soy, ${character.name}`)
 }
 
-var felipe = new Desarrollador('Felipe', 'Salamanca', 1.74)
-var erika = new Persona('Erika', 'Luna', 1.65)
-var arturo = new Desarrollador('Arturo','Martinez', 1.85) 
-
-felipe.saludar(responderSaludo)
-erika.saludar(responderSaludo)
-arturo.saludar(responderSaludo)
+$.get (lukeUrl, opts, onPeopleResponse)
