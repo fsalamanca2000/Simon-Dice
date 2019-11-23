af1135d9584843c37e3101db40598eeddd69c316
Felipe Salamanca <juanfe689@gmail.com>
2019-11-22 20:11:43 -0500

    Hacer un multiple request

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -1,10 +1,16 @@
 const API_URL ='https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
-
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
 const opts = { crossDomain: true}
+
 const onPeopleResponse = function (character){
-  console.log(`Hola yo soy, ${character.name}`)
+  console.log(`Hola yo soy ${character.name}`)
 }
 
-$.get (lukeUrl, opts, onPeopleResponse)
+function obtenerPersonaje(id){
+  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+  $.get (url, opts, onPeopleResponse)
+}
+
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
