3cf3d92ad0275c555654140b55aa87036a0838e7
Felipe Salamanca <juanfe689@gmail.com>
2019-11-26 22:26:55 -0500

    Async-await (Detener la ejecucion del problema)

--- JS/simon-dice.js
+++ JS/simon-dice.js
@@ -2,8 +2,6 @@ const API_URL ='https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 const opts = { crossDomain: true}
 
-
-
 function obtenerPersonaje(id){
 return new Promise((resolve,reject) =>{
 
@@ -18,45 +16,16 @@ return new Promise((resolve,reject) =>{
 function onError(id){
   console.log(`Sucedio un error al obtener el personaje ${id}`)
 }
+async function obtenerPersonajes(){
+  var ids = [1, 2, 3, 4, 5, 6, 7]
+  var promesas = ids.map(id => obtenerPersonaje(id))
+  try{
+    var characters = await Promise.all(promesas)
+    console.log(characters)
+  } catch(id){
+    onError(id)
+  }
+}
 
-var ids = [1, 2, 3, 4, 5, 6, 7]
-/* var promesas = ids.map(function(id){
-  return obtenerPersonaje
-}) */
-var promesas = ids.map(id => obtenerPersonaje(id))
-Promise
-  .all(promesas)
-  .then(character => console.log(character))
-  .catch(onError)
-/* obtenerPersonaje(1)
-  .then(character =>{
-    console.log(`El personaje 1 es ${character.name}`)
-    return obtenerPersonaje(2)
-  })
-  .then(character =>{
-    console.log(`El personaje 2 es ${character.name}`)
-    return obtenerPersonaje(3)
-  })
-  .then(character =>{
-    console.log(`El personaje 3 es ${character.name}`)
-    return obtenerPersonaje(4)
-  })
-  .then(character =>{
-    console.log(`El personaje 4 es ${character.name}`)
-    return obtenerPersonaje(5)
-  })
-  .then(character =>{
-    console.log(`El personaje 5 es ${character.name}`)
-    return obtenerPersonaje(6)
-  })
-  .then(character =>{
-    console.log(`El personaje 6 es ${character.name}`)
-    return obtenerPersonaje(7)
-  })
-  .then(character =>{
-    console.log(`El personaje 7 es ${character.name}`)
-  })
-  .catch(onError)
- */
-
+obtenerPersonajes()
 
